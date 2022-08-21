cdf251f4d2baf4fff3ff1184c34f295e2eaa9f06
foag1996 <foalvarezg@ufpso.edu.co>
2022-08-21 18:51:16 -0500

    heroku

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ from typing import Union
 import requests
 from fastapi import FastAPI, status, Response
 from fastapi import FastAPI
+import logging.config
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
+Instrumentator().instrument(app).expose(app)
+
+# setup loggers
+logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
+
+# get root logger
+logger = logging.getLogger(__name__)  # the __name__ resolve to "main" since we are at the root of the project. 
+                                      # This will get the root logger since no logger in the configuration has this name.
 
 
 @app.get("/")
